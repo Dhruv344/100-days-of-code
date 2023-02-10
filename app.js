5617be8724c630659152fa9971ef2c80e4a4bfb0
Dhruv Jain <dhruvjain657@gmail.com>
2023-02-11 01:05:01 +0530

    Initialised commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let greetingText = 'Hi bitch! Here\'s your Daddy';
+alert(greetingText);
+greetingText = 'Didn\'t you listen?';
+alert(greetingText);
+alert(69);
+let hobbies = ['Gymming','Watching Movies','Coding','PowerLifting','Web Designing',44];
+
+alert(hobbies[1]);
